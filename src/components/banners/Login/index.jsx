bb6267c88955e511b6d084ea01dff6bee676fee9
M4G1Ck <gabrielmacedoaraujo@hotmail.com>
2023-12-12 15:05:54 -0300

    add: modal Login e Cadastro

--- src/components/banners/Login/index.jsx
+++ src/components/banners/Login/index.jsx
@@ -1,11 +1,25 @@
+import * as React from 'react';
+import * as Components from './Components';
 import styles from "./styles.module.css";
-import React from "react";
 import backgroundImage from "assets/images/firstAcessBackground.png";
 import tempLogoCard from "assets/images/tempLogo.svg";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Switch from '@mui/material/Switch';
 
-const UserCard = ({ children }) => {
+const UserCard = ({ children, onClick }) => {
   return (
-    <div className={styles.card}>
+    <button onClick={onClick} className={styles.card}>
       <div className={styles.imageBox2}>
         <img
           src={tempLogoCard}
@@ -14,15 +28,90 @@ const UserCard = ({ children }) => {
         <h4>{children}</h4>
       </div>
       <h3>{">"}</h3>
-    </div>
+    </button>
   );
 };
 
-export default function Login({ type }) {
-  // eslint-disable-next-line
+
+export default function MaxWidthDialogLogin({ type }) {
+  const [open, setOpen] = React.useState(false);
+  const [fullWidth, setFullWidth] = React.useState(true);
+  const [maxWidth, setMaxWidth] = React.useState('sm');
   const [filteredId, filteredBanner, background, revertPosition, anchor] = type;
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleMaxWidthChange = (event) => {
+    setMaxWidth(event.target.value);
+  };
+
+  const handleFullWidthChange = (event) => {
+    setFullWidth(event.target.checked);
+  };
+
+  const [signIn, toggle] = React.useState(true);
+
   return (
-    <>
+    <React.Fragment>
+      <Dialog
+        maxWidth={maxWidth}
+        open={open}
+        onClose={handleClose}
+      >
+        <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Criar Conta</Components.Title>
+                      <Components.Input type='text' placeholder='Nome' />
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Senha' />
+                      <Components.Button>Cadastrar</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
+
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>LOGIN</Components.Title>
+                       <Components.Input type='email' placeholder='Email' />
+                       <Components.Input type='password' placeholder='Senha' />
+                       <Components.Anchor href='#'>Esqueceu sua senha?</Components.Anchor>
+                       <Components.Button>ENTRAR</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title2>Bem vindo de volta!</Components.Title2>
+                      <Components.Paragraph>
+                        Para se manter conectado conosco, faça login com suas informações pessoais
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                        Entrar
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title2>Olá, Amigo!</Components.Title2>
+                        <Components.Paragraph>
+                          Insira seus dados pessoais e comece a jornada conosco
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Cadastre-se
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+          </Components.Container>
+          </Dialog>
       {filteredBanner.map((banner) => (
         <section className={styles.page}>
           <image
@@ -36,8 +125,8 @@ export default function Login({ type }) {
             </div>
 
             <div className={styles.cards}>
-              <UserCard children={"Entrar como colaborador"} />
-              <UserCard children={"Entrar como visitante"} />
+              <UserCard onClick={handleClickOpen} children={"Entrar como colaborador"} />
+              <UserCard onClick={handleClickOpen} children={"Entrar como visitante"} />
             </div>
 
             <a
@@ -49,6 +138,9 @@ export default function Login({ type }) {
           </div>
         </section>
       ))}
-    </>
+    </React.Fragment>
   );
 }
+
+
+
